f2a346d01815a5f6c35d243e5fc09c60f42262ab
JohnAsare <johnasare7@gmail.com>
2020-06-07 13:47:47 -0400

    Add another fucntion called myfunc_one to solve the problem with a different strategy

--- Skyline.py
+++ Skyline.py
@@ -7,11 +7,22 @@ and odd letters as lowercase"""
 
 
 def myfunc(statement):
-    for index, letter in enumerate(statement):
+    for index, letter in enumerate(statement, 1):
         if index % 2 == 0:
             print(letter.upper())
         else:
             print(letter.lower())
 
 
-print(myfunc("Thisisaboy"))
+def myfunc_one(text):
+    processed = ''
+    for i in range(len(text)):
+        if i % 2 == 0:
+            processed += text[i].lower()
+        else:
+            processed += text[i].upper()
+    return processed
+
+
+print(myfunc_one("Anthropomorphism"))
+print(myfunc("Anthropomorphism"))

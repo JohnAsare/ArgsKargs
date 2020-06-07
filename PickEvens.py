8e0f7ee778a2ffa477d7b9fe2b6c13e74a0c73ee
JohnAsare <johnasare7@gmail.com>
2020-06-07 12:52:38 -0400

    Add return code to fix the return statement. The code now prints even numbers

--- PickEvens.py
+++ PickEvens.py
@@ -7,7 +7,7 @@ even numbers"""
 
 
 def myfunc(*args):
-    return [args for number in args if number % 2 == 0]
+    return [number for number in args if number % 2 == 0]
 
 
 print(myfunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))

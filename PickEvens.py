4f5471795153a14ece123e518626ca75125de218
JohnAsare <johnasare7@gmail.com>
2020-06-07 12:45:02 -0400

    Add myfunc function to return even number of arbitary numbers passed but it fails

--- PickEvens.py
+++ PickEvens.py
@@ -5,5 +5,9 @@
 """ Problem: Define a function that takes in many arguments but returns a list of those that just
 even numbers"""
 
-numbers = 10, 20, 30
-print(list(numbers))
+
+def myfunc(*args):
+    return [args for number in args if number % 2 == 0]
+
+
+print(myfunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))

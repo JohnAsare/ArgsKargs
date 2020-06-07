e9af37a99ddf5502d2570ef9b8c693cd5fc46203
JohnAsare <johnasare7@gmail.com>
2020-06-07 13:33:34 -0400

    Add myfunc function to return letters that are even as uppercase and odds as lowercase

--- Skyline.py
+++ Skyline.py
@@ -1,3 +1,17 @@
 # John Asare
 # Jun 7 2020 12:57
 
+
+""" Question: Define a function that takes in string and returns all letters that are even as uppercase
+and odd letters as lowercase"""
+
+
+def myfunc(statement):
+    for index, letter in enumerate(statement):
+        if index % 2 == 0:
+            print(letter.upper())
+        else:
+            print(letter.lower())
+
+
+print(myfunc("Thisisaboy"))
